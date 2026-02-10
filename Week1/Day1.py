4121e9f614b8e7d34dd03f658fb9d5e2da28f504
Hashir Ahmad <hashirahmad806@gmail.com>
2026-02-11 01:08:35 +0500

    Updated project

--- Week1/Day1.py
+++ Week1/Day1.py
@@ -1,1 +1,20 @@
+
 print("Hello   World   Its  Me  Hashir Ahmad") ;
+  
+print("Hellow  world" , end= "")
+print("  This   will  print  in the Same  Line ")
+
+#  variables  in  pythons
+variable = 5
+
+print(variable)
+
+# mathematical   operations
+num1 = 7
+num2 =8 
+
+print("Addition is " , num1 + num2) 
+print(  "Subtraction  is "  , num1 - num2) 
+print(  "Division   is "  , num1 / num2) 
+print( "Multiplication  is "  , num1 * num2) 
+ 
