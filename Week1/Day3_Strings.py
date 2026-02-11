a13fcc60bbff29dfbfa17b9ec631b00f51e0650d
Hashir Ahmad <hashirahmad806@gmail.com>
2026-02-11 23:50:40 +0500

    Updated project

---
+++ Week1/Day3_Strings.py
@@ -0,0 +1,24 @@
+# Sclicing  of  Strings
+Str1 = ' University'
+# sli = slice(Str1[2:5])
+print(Str1[1:6]) #Unive 
+
+#  case conversion  
+uper =Str1.upper() # UNIVERSITY
+print(uper)
+
+Lower =Str1.lower();
+
+print(Lower); 
+
+print(Str1.strip()) #  Remove  Extera Spaces
+
+#  formate  string means  when   you want to add  some   number  Between Strings
+
+Age = 44
+print(f"  i am     {Age} years  old")
+session =2020_2028
+
+uni = f"I am  Studying  At uet Peshawar at Batch {session} "
+
+print(uni)
