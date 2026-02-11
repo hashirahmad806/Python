fb019d07edc1a15067af34f2f48a08a67c8aba5f
Hashir Ahmad <hashirahmad806@gmail.com>
2026-02-11 23:14:30 +0500

    Updated project

--- Week1/Day1.py
+++ Week1/Day1.py
@@ -17,4 +17,30 @@ print("Addition is " , num1 + num2)
 print(  "Subtraction  is "  , num1 - num2) 
 print(  "Division   is "  , num1 / num2) 
 print( "Multiplication  is "  , num1 * num2) 
+
+  # finding  types  of   variables
+  
+a = 100
+b = ' Hashir Ahmad'
+c = 455.5
+d = 44/22
+
+print(type(a)) #will  show  integer  class  this  means that is  belongs to the  integer numbers 
+print(type(a))#<class 'int'>
+
+print(type(b))#<class 'str'>
+
+print(type(c))#<class 'float'>
+
+print(type(d))#<class 'float'>
+
+
+
+ # type casting means changing the  type  of  varibles
+ 
+print(int(c)) #<class 'int'> 455
+print(float(a)) #<class 'float'> 100.0
+
+
+
  
---
+++ Week1/Day2_Conditional_Statement.py
@@ -0,0 +1,14 @@
+#Conditional Statement means  uses of  if-else    elseIf and 
+
+condition = 20 >5
+Option1 = "No"
+Option2 = "Yes"
+
+
+
+if( condition ):
+   print("  20 IS  greater the 5"  )
+else:
+   print("smaller ")
+
+ 
